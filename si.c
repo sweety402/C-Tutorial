a16fb7ba4edb7a84ef288b4cacb0e4d10f6c07a5
Aman <125014845+sweety402@users.noreply.github.com>
2024-12-27 23:30:59 +0530

    Create si.c

---
+++ si.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+int si (int principal,int rate,int time){
+    int simple_interest=(principal*rate*time)/100;
+    return simple_interest;
+}
+int main(){
+    int principal,rate,time;
+    printf("enter:pricipal,rate %,time year");
+    scanf("%d,%d,%d",&principal,&rate,&time);
+    printf("simple interest:%d",si(principal,rate,time));
+    return 0;
+}
