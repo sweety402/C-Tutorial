2c906cb9e95c75a9ef547889965608aaa55868b2
Aman <125014845+sweety402@users.noreply.github.com>
2024-12-27 23:59:17 +0530

    Create product.c

---
+++ product.c
@@ -0,0 +1,19 @@
+// write a program to return product of two number 
+#include "stdio.h"
+int main()
+{
+    int a;
+    int b;
+    printf("enter first number = ");
+    scanf("%d",&a);
+    printf("enter second number = ");
+    scanf("%d",&b);
+    printf("product:%d",a*b);
+    return 0;
+}
+
+
+// output
+// enter first number = 20
+// enter second number = 5
+// product:100
