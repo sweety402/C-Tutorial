e87c96bbdbe8f0690e73d75bab9b9db744686b59
Aman <125014845+sweety402@users.noreply.github.com>
2026-02-01 05:29:01 +0530

    Create c calculator.c

---
+++ c calculator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int a;
+    int b;
+    printf("enter the first number = a =");
+    scanf("%d",&a);
+    printf("enter the second number = b =");
+    scanf("%d",&b);
+    printf("divide:%d\n",a/b);
+    printf("product:%d\n",a*b);
+    printf("sum:%d\n",a+b);
+    printf("substract:%d",a-b);
+    return 0;
+}
